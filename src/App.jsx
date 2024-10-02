74cec5d06494500adf56cf1893e0dbae47b49ddc
Rick Rieta <rickrieta@gmail.com>
2024-10-02 14:05:05 -0700

    Use hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 import { routes } from './routes'
 import './index.css'
 
-const router = createBrowserRouter(routes)
+const router = createHashRouter(routes)
 
 const App = () => {
   return (
